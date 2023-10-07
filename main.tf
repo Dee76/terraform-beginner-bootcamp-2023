7779c2be6577ea37ed8699d346f0e26dc296638b
Dee Abson <dee.abson@gmail.com>
2023-10-07 04:44:07 -0600

    #48 Test TerraHome on TerraTowns test town. (#49)

--- main.tf
+++ main.tf
@@ -22,29 +22,28 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
-resource "terratowns_home" "home" {
+resource "terratowns_home" "lebowski_home" {
   name = "The Big Lebowski: A Philisophical Analysis"
   description = <<DESCRIPTION
 The Big Lebowski is not only a cult comedy, but an interesting study in philisophical ideas.
 Here we will introduce some of these ideas, along with showcasing the rise of Dudeism.
 DESCRIPTION
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3xf332sdfs.cloudfront.net"
-  town = "video-valley"
-  content_version = 1
+  domain_name = module.terrahouse_aws.cloudfront_url
+  # domain_name="3xf332sdfs.cloudfront.net"
+  town = "missingo"
+  content_version = var.content_version
 }
